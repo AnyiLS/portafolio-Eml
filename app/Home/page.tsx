8741d6f1d9ad1f589c9bbca8b409f3f660c0740c
AnyiLS <tdprogramacioneml5@gmail.com>
2024-05-20 07:12:25 -0500

    commit final

---
+++ app/Home/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Link href='/login'>hola</Link>
+    </div>
+  )
+}
+
+export default Home
